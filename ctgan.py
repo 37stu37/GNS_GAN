c7754081c2b2ed37b02a69de6abcf2c75eb83e5c
add <60672071+37stu37@users.noreply.github.com>
2020-06-04 17:47:58 +1200

    commit with GAN results (1000 samples)

--- ctgan.py
+++ ctgan.py
@@ -12,6 +12,7 @@ data = pd.read_csv('FFEsample1perc.csv')
 data.drop(['Unnamed: 0', 'scenario', 'pid'], axis=1, inplace=True)
 
 sample = data.sample(frac=0.01)
+print(len(sample))
 
 discrete_columns = ['source', 'target']
 
@@ -22,4 +23,4 @@ e=1
 ctgan.fit(sample, discrete_columns, epochs=e)
 endFit_time = time.time()-fit_time
 print("{} epochs finished in {}".format(e, endFit_time))
-GANsamples = ctgan.sample(100000)
+GANsamples = ctgan.sample(1000)
